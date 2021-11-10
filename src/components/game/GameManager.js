dc7a5d4f25006c2146f3e5d643015cc50c0eb2cf
Alexander Schroyer <alexanderschroyer@gmail.com>
2021-11-10 15:08:36 -0600

    reviews edit

--- src/components/game/GameManager.js
+++ src/components/game/GameManager.js
@@ -26,3 +26,24 @@ export const getCategories = () => {
     })
         .then(response => response.json())
 }
+
+export const getReviews = () => {
+    return fetch("http://localhost:8000/reviews", {
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("gr_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+
+
+export const createReview= (review) => {
+    return fetch("http://localhost:8000/reviews", {
+        method: "POST",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("gr_token")}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(review)
+    }).then(getReviews)        
+}
--- src/components/game/ReviewForm.js
+++ src/components/game/ReviewForm.js
@@ -1,42 +1,78 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { getCurrentUser } from "../APIManager";
+import { createReview, getReviews, getGames } from "./GameManager";
+
+var currentDate = new Date(Date.now() + (8 * 86400000))
+var newDateOptions = {
+        day: "2-digit",
+        year: "numeric",
+        month: "2-digit"
+}
+var newDate = currentDate.toLocaleString("en-US", newDateOptions );
 
 export const ReviewForm = () => {
-    const [review, setReview] = useState([])
+    const [game, setGame] = useState([])
     const history = useHistory()
     const currentUser = getCurrentUser()
+    const [currentReview, setCurrentReview] = useState({
+        gameId: 0,
+        content: "",
+        createdOn: ""
+    })
 
-    const getReviews = () => {
-        return fetch("http://localhost:8000/reviews", {
-            headers:{
-                "Authorization": `Token ${localStorage.getItem("gr_token")}`
-            }
-        })
-            .then(response => response.json())
-            .then((data) => {
-                setReview(data)
-            })
-    }
+    useEffect(() => {
+        getGames()
+        .then(data => setGame(data))
+    }, [])
     
     const { gameId } = useParams()
     
 
-    useEffect(
-        () => {
-            getReviews()
-        },
-        []
-    )
+    // useEffect(
+    //     () => {
+    //         getReviews()
+    //     },
+    //     []
+    // )
+
+    const changeReviewState = (event) => {
+        const newReviewState = { ...currentReview }
+        newReviewState[event.target.name] = event.target.value
+        setCurrentReview(newReviewState)
+    }
 
     return (
         <>
-            <div className="review__box">
-                <div className="review__game">Game: {review.game?.title}</div>
-                <div className="review__rater">Rater: {review.rater?.user?.username}</div>
-                <div className="review__content">Review: {review.content}</div>
-                <div className="review__createdOn">Created On: {review.created_on}</div>
-            </div>
+            <form className="reviewForm">
+            <h2 className="gameReview__title">Leave Review</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="content">Content: </label>
+                    <input type="text" name="content" required autoFocus className="form-control"
+                        value={currentReview.content}
+                        onChange={changeReviewState}
+                    />
+                </div>
+            </fieldset>
+
+            <button type="submit"
+                onClick={evt => {
+                    // Prevent form from being submitted
+                    evt.preventDefault()
+
+                    const review = {
+                        gameId: parseInt(gameId),
+                        content: currentReview.content,
+                        createdOn: newDate
+                    }
+
+                    // Send POST request to your API
+                    createReview(review)
+                        .then(() => history.push(`/game/${gameId}/review`))
+                }}
+                className="btn btn-primary">Post</button>
+            </form>
         </>
     )
 }
